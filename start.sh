02b90adccaabbcf44df8b757e7942a10527888f6
Bruno Morais <brunomoraisads@gmail.com>
2023-07-25 19:27:04 -0300

    Comando script para rodar após o container iniciar

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #1/bin/bash
 
+# Comando para instalação da node_modules
 npm install 
 
+# Comando para rodar a migration da aplicação
+npx prisma db push
+
+# Comando para manter o terminal aberto para receber comandos no terminal
 tail -f /dev/null
 
-#npx prisma db push & npm run start:dev
+# npm run start:dev
